6b366b3a7e84df693799c014508b937a75e742ec
Alina <alinatussupova@gmail.com>
2023-04-03 13:00:58 +0600

    Creating a Form Model for job_application.

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .forms import ApplicationForm
 
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            status = form.cleaned_data["status"]
+            print(first_name)
+
+
+
     return render(request, "index.html")
